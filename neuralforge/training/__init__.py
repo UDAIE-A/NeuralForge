c643f61236c74f41fc71043333b34a745ff8c9ac
UDAIE-A <udayfluer@gmail.com>
2026-06-03 23:35:31 +0530

    fix: export create_dataloaders in training init

--- neuralforge/training/__init__.py
+++ neuralforge/training/__init__.py
@@ -1,4 +1,4 @@
 from .trainer import Trainer
-from .data import TextDataset, DataLoader
+from .data import TextDataset, DataLoader, create_dataloaders
 
-__all__ = ['Trainer', 'TextDataset', 'DataLoader']
+__all__ = ['Trainer', 'TextDataset', 'DataLoader', 'create_dataloaders']
