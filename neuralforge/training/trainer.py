f22261500b7033cb2cdb4f88179e2debfa4f9aa5
UDAIE-A <udayfluer@gmail.com>
2026-06-04 01:03:02 +0530

    fix: correct GPU memory attribute name

--- neuralforge/training/trainer.py
+++ neuralforge/training/trainer.py
@@ -147,7 +147,7 @@ class Trainer:
     def _print_header(self, num_epochs: int):
         """Print training header."""
         gpu_name = torch.cuda.get_device_name(0)
-        gpu_mem = torch.cuda.get_device_properties(0).total_mem / 1024**3
+        gpu_mem = torch.cuda.get_device_properties(0).total_memory / 1024**3
         n_params = self.model.count_parameters()
         
         print()
